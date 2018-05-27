e9fa019bfacc945d61d87c1d69b639f093341902
Tricker <Tricker@tricker.cn>
2018-05-27 20:13:21 +0800

    Work with goods

---
+++ app/Catalog.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Catalog extends Model
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    protected $fillable = [
+        'catalog',
+    ];
+}
--- database/migrations/2018_05_27_110001_create_catalogs_table.php
+++ database/migrations/2018_05_27_110001_create_catalogs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGoodsTable extends Migration
+class CreateCatalogsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreateGoodsTable extends Migration
      */
     public function up()
     {
-        Schema::create('goods', function (Blueprint $table) {
+        Schema::create('catalogs', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('catalog')->unique();
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ class CreateGoodsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('goods');
+        Schema::dropIfExists('catalogs');
     }
 }
